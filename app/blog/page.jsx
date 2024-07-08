f09f5f5b4ccec0ed826d471cfe26d3b130fa852f
taleenteen <teenteenza1@hotmail.com>
2024-07-08 20:09:20 +0700

    add crud system like name picture and details as basic mongoose

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,10 +1,69 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import Image from "next/image";
+import Link from 'next/link'
+import DeleteBtn from '../components/Post/DeleteBtn';
 import React from 'react'
 
 export default function BlogPage() {
+  const [postData, setPostData] = useState([]);
+
+  console.log(postData);
+
+  const getPosts = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/posts", {
+        cache: "no-store"
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch posts");
+      }
+
+      const data = await res.json();
+      setPostData(data.posts);
+
+    } catch(error) {
+      console.log("Error loading posts: ", error);
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div className='container mx-auto min-h-screen bg-gray-100'>
-      <h1>Sawat d kub p</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque atque veniam possimus beatae voluptates consequuntur ex recusandae nesciunt, corporis rem.</p>
-    </div>
+    <main className="container mx-auto">
+     <h3>NextJS Crud</h3>
+     <hr className="my-3" />
+     <button className="bg-green-500 p-3 text-white rounded"><Link href="/create-post">Create Post</Link></button>
+     
+     <div className="grid grid-cols-4 mt-3 gap-5">
+      {postData && postData.length > 0 ? (
+        postData.map(val => (
+          <div key={val._id} className='shadow-xl my-10 p-10 rounded-xl'>
+            <h4 className='text-2xl'>{val.title}</h4>
+            <img
+        className='my-3 rounded-md'
+        src={val.img}
+        width={300}
+        height={0}
+        alt={val.title}
+      />     
+            <p>{val.content}</p>
+            <div className='mt-5'>
+              <Link className='bg-gray-500 text-white border py-2 px-3 rounded-md text-lg my-2' href={`/edit-post/${val._id}`}>Edit</Link>
+              <DeleteBtn id={val._id} />
+            </div>
+          </div>
+        ))
+      ) : (
+        <p className='bg-gray-300 p-3 mt-3'>
+          You do not have any posts yet.
+        </p>
+      )}
+     </div>
+    </main>
   )
 }
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,44 +1,40 @@
 "use client"
-import React from 'react'
+import React, { useState } from "react";
 import Link from 'next/link'
 import { signOut } from 'next-auth/react'
 import useSessionData from './Session'
-
+import "./Navbar.css";
 
 function Nav() {
   const { session, status } = useSessionData();
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav className='bg-[#333] text-white p-5'>
-        <div className='container mx-auto'>
-            <div className='flex justify-between items-center'>
-                <div>
-                <li><Link href="/">Home</Link></li>
-                </div>
-                <ul className='flex'>
-      <li className='mx-3'><Link href="about">About</Link></li>
-      <li className='mx-3'><Link href="services">Services</Link></li>
-      <li className='mx-3'><Link href="blog">Blog</Link></li>
-      <li className='mx-3'><Link href="contact">Contact</Link></li>
-      {!session ? (
-        <>
-        <li className='mx-3 text-green-200'><Link href="login">Login</Link></li>
-        <li className='mx-3 text-blue-200'><Link href="register">Register</Link></li>
-        </>
-      ) : (
-      <>
-      <li className='mx-3 text-white '><a href='/welcome' className='bg-gray-500 border py-2 px-3 rounded-md text-lg my-2'>Profile</a></li>
-      <li className='mx-3 text-white '><a onClick={()=> signOut()} className='bg-red-500 border py-2 px-3 rounded-md text-lg my-2'>LogOut</a></li>
-      </>
-      )
-    }
-      
-    </ul>
-            </div>
-        
-        </div>
+    <nav>
+      <li><Link href="/">Home</Link></li>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li><Link href="about">About</Link></li>
+        <li><Link href="services">Services</Link></li>
+        <li><Link href="contact">Contact</Link></li>
+        <li><Link href="blog">Blog</Link></li>
+        {!session ? (
+          <>
+            <li className='text-green-200'><Link href="login">Login</Link></li>
+            <li className='text-blue-200'><Link href="register">Register</Link></li>
+          </>
+        ) : (
+          <>
+            <li className='text-white'><a href='/welcome' className='bg-gray-500 border  rounded-md  '>Profile</a></li>
+            <li className='text-white'><a onClick={() => signOut()} className='bg-red-500 border rounded-md  '>LogOut</a></li>
+          </>
+        )}
+      </ul>
     </nav>
-    
-  )
+  );
 }
 
-export default Nav
+export default Nav;
